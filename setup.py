a46528253251d64a852885ef9cfcb91f8fe21e0c
Paul Slusarz <paulslusarz@carfax.com>
2024-05-26 21:25:01 -0500

    first commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+
+setuptools.setup(
+    name="mugbook",
+    version="0.1.0",
+    author="Paul Slusarz",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.12',
+)
